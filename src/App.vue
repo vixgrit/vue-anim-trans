c2806830529e1c4040b1a52a6ea8265ce15ba8c9
Jonathan Kvicky <root.vixgrit@gmail.com>
2017-06-12 22:07:41 -0400

    use transition group to animate a list

--- src/App.vue
+++ src/App.vue
@@ -59,6 +59,21 @@
         <transition name="fade" mode="out-in">
           <component :is="selectedComponent"></component>
         </transition>
+        <hr>
+        <button class="btn btn-primary" type="button" name="button" @click="addItem">Add Item</button>
+        <br><br>
+        <ul class="list-group">
+          <transition-group name="slide">
+            <li
+              class="list-group-item"
+              v-for="(number, index) in numbers"
+              @click="removeItem(index)"
+              style="cursor: pointer;"
+              :key="number">
+            {{ number }}
+            </li>
+          </transition-group>
+        </ul>
       </div>
     </div>
   </div>
@@ -78,10 +93,18 @@
         load: true,
         alertAnimation: 'fade',
         elementWidth: 100,
-        selectedComponent: 'app-success-alert'
+        selectedComponent: 'app-success-alert',
+        numbers: [1, 2, 3, 4, 5]
       }
     },
     methods: {
+      addItem() {
+        const pos = Math.floor(Math.random() * this.numbers.length);
+        this.numbers.splice(pos, 0, this.numbers.length + 1);
+      },
+      removeItem(index) {
+        this.numbers.splice(index, 1);
+      },
       beforeEnter(el) {
         console.log('beforeEnter');
         this.elementWidth = 100;
@@ -168,6 +191,11 @@
     animation: slide-out 1s ease-out forwards;
     transition: opacity 1s;
     opacity: 0;
+    position: absolute;
+  }
+
+  .slide-move {
+    transition: transform 1s;
   }
 
   @keyframes slide-in {
